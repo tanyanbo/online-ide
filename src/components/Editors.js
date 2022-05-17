d91e8b9021ca88e887a5e0925d50433438ff87e8
Tan Yanbo <tanyanbo95@hotmail.com>
2022-05-17 22:10:06 +0800

    Still implementing drag and drop

--- src/components/Editors.js
+++ src/components/Editors.js
@@ -1,33 +1,91 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Editor from "./Editor";
 import { LANGUAGES } from "../Languages";
 
 const Editors = () => {
-  const [startingX, setStartingX] = useState(0);
-  const [startingY, setStartingY] = useState(0);
-  const [isDragging, setIsDragging] = useState(false);
+  const [isDragging, setIsDragging] = useState(0);
   const leftDraggableBarRef = useRef(null);
   const rightDraggableBarRef = useRef(null);
   const leftEditorRef = useRef(null);
   const rightEditorRef = useRef(null);
   const centerEditorRef = useRef(null);
+  const containerRef = useRef(null);
 
-  const handleMouseDown = (e) => {
-    setStartingX(e.clientX);
-    setStartingY(e.clientY);
-    setIsDragging(true);
+  let x;
+
+  const handleMouseDownLeft = (e) => {
+    x = e.clientX;
+    setIsDragging(1);
+  };
+
+  const handleMouseDownRight = (e) => {
+    x = e.clientX;
+    setIsDragging(2);
   };
 
   const handleMouseMove = (e) => {
-    if (isDragging) {
-      const dx = e.clientX - startingX;
-      if (dx < 0) {
-        leftEditorRef.current.style.width = `${
-          leftEditorRef.current.offsetWidth - 10
-        }px`;
-        leftDraggableBarRef.current.style.width = `${leftDraggableBarRef.current.offsetWidth}px`;
-      }
+    if (isDragging === 0) return;
+    const containerWidth = containerRef.current.offsetWidth;
+    const barWidth = leftDraggableBarRef.current.offsetWidth;
+    const leftBar = leftDraggableBarRef.current;
+    const rightBar = rightDraggableBarRef.current;
+    const center = centerEditorRef.current;
+    const left = leftEditorRef.current;
+    const right = rightEditorRef.current;
+    x = e.clientX;
+    if (isDragging === 1) {
+      leftBar.style.position = "absolute";
+      leftBar.style.left = x + "px";
+      leftBar.style.backgroundColor = "black";
+      leftBar.style.height = `${left.offsetHeight}px`;
+      left.style.width = `${x}px`;
+      left.style.flexGrow = "0";
+      center.style.position = "absolute";
+      center.style.left = `${left.offsetWidth + barWidth}px`;
+      center.style.flexGrow = "0";
+      center.style.right = `${
+        (containerWidth - 2 * barWidth) / 3 + barWidth
+      }px`;
+      rightBar.style.position = "absolute";
+      rightBar.style.left = `${
+        left.offsetWidth + barWidth + center.offsetWidth
+      }px`;
+      rightBar.style.backgroundColor = "black";
+      rightBar.style.height = `${left.offsetHeight}px`;
+      right.style.position = "absolute";
+      right.style.flexGrow = "0";
+      right.style.left = `${
+        left.offsetWidth + 2 * barWidth + center.offsetWidth
+      }px`;
+      right.style.width = `${
+        containerWidth - 2 * barWidth - center.offsetWidth - left.offsetWidth
+      }px`;
+      return;
     }
+    leftBar.style.position = "absolute";
+    leftBar.style.left = x + "px";
+    leftBar.style.backgroundColor = "black";
+    leftBar.style.height = `${left.offsetHeight}px`;
+    left.style.width = `${x}px`;
+    left.style.flexGrow = "0";
+    center.style.position = "absolute";
+    center.style.left = `${left.offsetWidth + barWidth}px`;
+    center.style.flexGrow = "0";
+    center.style.right = `${(containerWidth - 2 * barWidth) / 3 + barWidth}px`;
+    rightBar.style.position = "absolute";
+    rightBar.style.left = `${
+      left.offsetWidth + barWidth + center.offsetWidth
+    }px`;
+    rightBar.style.backgroundColor = "black";
+    rightBar.style.height = `${left.offsetHeight}px`;
+    right.style.position = "absolute";
+    right.style.flexGrow = "0";
+    right.style.left = `${
+      left.offsetWidth + 2 * barWidth + center.offsetWidth
+    }px`;
+    right.style.width = `${
+      containerWidth - 2 * barWidth - center.offsetWidth - left.offsetWidth
+    }px`;
   };
 
   const handleMouseUp = (e) => {
@@ -35,20 +93,23 @@ const Editors = () => {
   };
 
   return (
-    <div className="editor-container" onMouseUp={handleMouseUp}>
+    <div
+      className="editor-container"
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+      ref={containerRef}
+    >
       <Editor language={LANGUAGES.HTML} myRef={leftEditorRef} />
       <div
         ref={leftDraggableBarRef}
         className="resize-bar"
-        onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
+        onMouseDown={handleMouseDownLeft}
       ></div>
       <Editor language={LANGUAGES.CSS} myRef={centerEditorRef} />
       <div
         ref={rightDraggableBarRef}
         className="resize-bar"
-        onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
+        onMouseDown={handleMouseDownRight}
       ></div>
       <Editor language={LANGUAGES.JS} myRef={rightEditorRef} />
     </div>
